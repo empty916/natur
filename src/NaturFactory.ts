b086d9cdb8f12f1130b39c146ab0d814879e3cfd
empty916 <empty916@qq.com>
2023-07-03 21:49:57 +0800

    1

---
+++ src/NaturFactory.ts
@@ -0,0 +1,213 @@
+import type { AllWatchAPI, AllWatchEvent, Maps, State, WatchAPI, WatchEvent } from './ts';
+import { ThunkParams } from './middlewares';
+
+export class NaturFactory {
+	/**
+	 * build a actions creator
+	 * @param state state of module
+	 * @param maps maps of module(optional)
+	 * @returns return a actions creator
+	 */
+	static actionsCreator<S extends State, M extends Maps = Maps>(state: S, maps?: M) {
+		function createActions<
+			A extends Record<
+				string,
+				| ((...args: any[]) => (p: ThunkParams<S, M extends Maps ? M : Maps>) => Partial<S> | void | Promise<Partial<S> | void>)
+				| ((...args: any[]) => Partial<S> | void | Promise<Partial<S> | void>)
+			>,
+		>(actions: A) {
+			return actions;
+		}
+		return createActions;
+	}
+	/**
+	 * build a map creator
+	 * @param state state of module
+	 * @returns return a map creator
+	 */
+	static mapCreator<S extends State>(state: S) {
+		function createMap<MD1 extends (s: S) => any>(m: MD1): MD1;
+		function createMap<MD1 extends (s: S) => any, F extends (p1: ReturnType<MD1>) => any>(...m: [MD1, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			F extends (p1: ReturnType<MD1>, p2: ReturnType<MD2>) => any,
+		>(...m: [MD1, MD2, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			F extends (p1: ReturnType<MD1>, p2: ReturnType<MD2>, p3: ReturnType<MD3>) => any,
+		>(...m: [MD1, MD2, MD3, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			F extends (p1: ReturnType<MD1>, p2: ReturnType<MD2>, p3: ReturnType<MD3>, p4: ReturnType<MD4>) => any,
+		>(...m: [MD1, MD2, MD3, MD4, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			MD5 extends (s: S) => any,
+			F extends (
+				p1: ReturnType<MD1>,
+				p2: ReturnType<MD2>,
+				p3: ReturnType<MD3>,
+				p4: ReturnType<MD4>,
+				p5: ReturnType<MD5>,
+			) => any,
+		>(...m: [MD1, MD2, MD3, MD4, MD5, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			MD5 extends (s: S) => any,
+			MD6 extends (s: S) => any,
+			F extends (
+				p1: ReturnType<MD1>,
+				p2: ReturnType<MD2>,
+				p3: ReturnType<MD3>,
+				p4: ReturnType<MD4>,
+				p5: ReturnType<MD5>,
+				p6: ReturnType<MD6>,
+			) => any,
+		>(...m: [MD1, MD2, MD3, MD4, MD5, MD6, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			MD5 extends (s: S) => any,
+			MD6 extends (s: S) => any,
+			MD7 extends (s: S) => any,
+			F extends (
+				p1: ReturnType<MD1>,
+				p2: ReturnType<MD2>,
+				p3: ReturnType<MD3>,
+				p4: ReturnType<MD4>,
+				p5: ReturnType<MD5>,
+				p6: ReturnType<MD6>,
+				p7: ReturnType<MD7>,
+			) => any,
+		>(...m: [MD1, MD2, MD3, MD4, MD5, MD6, MD7, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			MD5 extends (s: S) => any,
+			MD6 extends (s: S) => any,
+			MD7 extends (s: S) => any,
+			MD8 extends (s: S) => any,
+			F extends (
+				p1: ReturnType<MD1>,
+				p2: ReturnType<MD2>,
+				p3: ReturnType<MD3>,
+				p4: ReturnType<MD4>,
+				p5: ReturnType<MD5>,
+				p6: ReturnType<MD6>,
+				p7: ReturnType<MD7>,
+				p8: ReturnType<MD8>,
+			) => any,
+		>(...m: [MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			MD5 extends (s: S) => any,
+			MD6 extends (s: S) => any,
+			MD7 extends (s: S) => any,
+			MD8 extends (s: S) => any,
+			MD9 extends (s: S) => any,
+			F extends (
+				p1: ReturnType<MD1>,
+				p2: ReturnType<MD2>,
+				p3: ReturnType<MD3>,
+				p4: ReturnType<MD4>,
+				p5: ReturnType<MD5>,
+				p6: ReturnType<MD6>,
+				p7: ReturnType<MD7>,
+				p8: ReturnType<MD8>,
+				p9: ReturnType<MD8>,
+			) => any,
+		>(...m: [MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, MD9, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			MD5 extends (s: S) => any,
+			MD6 extends (s: S) => any,
+			MD7 extends (s: S) => any,
+			MD8 extends (s: S) => any,
+			MD9 extends (s: S) => any,
+			MD10 extends (s: S) => any,
+			F extends (
+				p1: ReturnType<MD1>,
+				p2: ReturnType<MD2>,
+				p3: ReturnType<MD3>,
+				p4: ReturnType<MD4>,
+				p5: ReturnType<MD5>,
+				p6: ReturnType<MD6>,
+				p7: ReturnType<MD7>,
+				p8: ReturnType<MD8>,
+				p9: ReturnType<MD8>,
+			) => any,
+		>(...m: [MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, MD9, MD10, F]): F;
+		function createMap<
+			MD1 extends (s: S) => any,
+			MD2 extends (s: S) => any,
+			MD3 extends (s: S) => any,
+			MD4 extends (s: S) => any,
+			MD5 extends (s: S) => any,
+			MD6 extends (s: S) => any,
+			MD7 extends (s: S) => any,
+			MD8 extends (s: S) => any,
+			MD9 extends (s: S) => any,
+			MD10 extends (s: S) => any,
+			MD11 extends (s: S) => any,
+			F extends (
+				p1: ReturnType<MD1>,
+				p2: ReturnType<MD2>,
+				p3: ReturnType<MD3>,
+				p4: ReturnType<MD4>,
+				p5: ReturnType<MD5>,
+				p6: ReturnType<MD6>,
+				p7: ReturnType<MD7>,
+				p8: ReturnType<MD8>,
+				p9: ReturnType<MD8>,
+			) => any,
+		>(...m: [MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, MD9, MD10, MD11, F]): F;
+		function createMap(...m: any[]) {
+			return m;
+		}
+		return createMap;
+	}
+	static watchCreator() {
+		function createWatch<
+			W extends
+				| Record<string, (event: WatchEvent<any>, api: WatchAPI<any, any>) => any>
+				| ((event: AllWatchEvent, api: AllWatchAPI) => any),
+		>(w: W) {
+			return w;
+		}
+		return createWatch;
+	}
+	/**
+	 * create a watch object of a natur module
+	 * @param w
+	 * @returns
+	 */
+	static createWatch<
+		W extends
+			| Record<string, (event: WatchEvent<any>, api: WatchAPI<any, any>) => any>
+			| ((event: AllWatchEvent, api: AllWatchAPI) => any),
+	>(w: W) {
+		return w;
+	}
+}
---
+++ test/NaturFactory.test.ts
@@ -0,0 +1,104 @@
+import { NaturFactory } from './../src/NaturFactory';
+
+
+
+const state = {
+    name: 'tom',
+    data: {
+        sex: 'male',
+        age: 18,
+    },
+    todo: [
+        {
+            id: 1,
+            name: 'play game'
+        },
+    ]
+}
+
+test('map creator', () => {
+    const createMap = NaturFactory.mapCreator(state);
+
+    const maps = {
+        f1: createMap(
+            s => s.data.age,
+        ),
+        f2: createMap(
+            s => s.data.age,
+            a => a + 1
+        ),
+        f3: createMap(
+            s => s.data.age,
+            s => s.todo[0].id,
+            (a, id) => a + id
+        ),
+    };
+
+    expect(maps).toMatchSnapshot();
+})
+
+test('actions creator', () => {
+    const createMap = NaturFactory.mapCreator(state);
+
+    const maps = {
+        f1: createMap(
+            s => s.data.age,
+        ),
+        f2: createMap(
+            s => s.data.age,
+            a => a + 1
+        ),
+        f3: createMap(
+            s => s.data.age,
+            s => s.todo[0].id,
+            (a, id) => a + id
+        ),
+    };
+    const createActions = NaturFactory.actionsCreator(state);
+
+    const actions1 = createActions({
+        a1: (name: string) => ({
+            name,
+        }),
+        a2: (sex: string) => (api) => ({
+            data: {
+                ...api.getState().data,
+                sex,
+            }
+        })
+    });
+    expect(actions1).toMatchSnapshot();
+
+    const createActions2 = NaturFactory.actionsCreator(state, maps);
+    const actions2 = createActions2({
+        addAge: () => (api) => ({
+            data: {
+                ...api.getState().data,
+                age: api.getMaps().f1,
+            }
+        })
+    });
+    expect(actions2).toMatchSnapshot();
+});
+
+test('watch creator', () => {
+    const createWatch = NaturFactory.watchCreator();
+    expect(createWatch({
+        ff: (event, api) => {
+            if (event.type === 'init') {
+                event.oldModule;
+            }
+        }
+    })).toMatchSnapshot();
+})
+
+
+test('create watch', () => {
+    expect(NaturFactory.createWatch({
+        ff: (event, api) => {
+            if (event.type === 'init') {
+                event.oldModule;
+            }
+        }
+    })).toMatchSnapshot();
+})
